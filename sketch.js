6db812ef69d7ac33e16d73b1055b6e1f7f0f8d1d
Wildjacky22 <62379@bwls.net>
2017-10-31 08:44:58 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,27 +1,43 @@
-function setup() {
+function setup(){
+  
   createCanvas( windowWidth , windowHeight );
-
-  noStroke();
   
-  background( 0 , 0 , 0 )
+  angleMode(DEGREES);
+  
+  }
 }
-function draw() {
 
-  var r = random( 0 , 35 );
-  var g = random( 0 , 150 );
-  var b = random( 0 , 255 );
-  var a = random( 0 , 100 );
+function draw(){
+  
+  background( 51 );
 
-  fill( r , g , b , 2 ); 
-  ellipse( mouseX , mouseY , 20 , 20 );
   
-  fill( r , g , b , a );
-  ellipse( mouseX , mouseY , 30 , 30 );
+  var hr = hour();
+  var min = minute();
+  var sec = second();
   
-  fill( r , g , b , a );
-  ellipse( mouseX , mouseY , 40 , 40);
-   
-  fill( r , g , b , a );
-  ellipse( mouseX , mouseY , 50 , 50);
-   
+  strokeWeight( 5 );
+  stroke( 255 , 100 , 100 );
+  noFill();
+  let end1 = map( sec , 0 , 60 , 0 , 360);
+  arc( windowWidth*0.5 , windowHeight*0.5 , 300 , 300 , 0 , end1 );
+  
+  strokeWeight( 5 );
+  stroke( 100 , 255 , 100 );
+  noFill();
+  let end2 = map( min , 0 , 60 , 0 , 360);
+  arc( windowWidth*0.5 , windowHeight*0.5 , 280 , 280 , 0 , end2 );
+  
+  strokeWeight( 5 );
+  stroke( 200 , 100 , 255 );
+  noFill();
+  let end3 = map( hr , 0 , 60 , 0 , 360);
+  arc( windowWidth*0.5 , windowHeight*0.5 , 260 , 260 , 0 , end3 );
+  
+  fill( 255 , 255 ,255 );
+  noStroke();
+  textSize(25);
+  text(hr + ':' + min+':' + sec , windowWidth*0.457 , windowHeight*0.5 );
+  
+  };
 }
